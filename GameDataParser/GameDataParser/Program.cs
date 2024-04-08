a0647f1f55fe3565de4a119a5aa6c8b294aaecf1
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-04-08 19:58:34 +0300

    Implement global try-catch block.

--- GameDataParser/GameDataParser/Program.cs
+++ GameDataParser/GameDataParser/Program.cs
@@ -6,10 +6,17 @@ namespace GameDataParser
     {
         static void Main(string[] args)
         {
+            try 
+            { 
                 string fileName = UserInteraction.ReceiveUserInput();
                 JsonRepository jsonRepository = new JsonRepository(fileName);
                 List<VideoGame> videoGames = jsonRepository.ReadFile();
                 UserInteraction.PrintGames(videoGames);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
