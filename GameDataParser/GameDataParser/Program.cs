1230fbc4a3801992ddf94f9ea69afe7c6e5f300e
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-04-08 17:53:00 +0300

    Read JSON file and print the contents on screen, in a sunny day scenario.

--- GameDataParser/GameDataParser/Program.cs
+++ GameDataParser/GameDataParser/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace GameDataParser
+﻿using System.Text.Json;
+
+namespace GameDataParser
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter the file name you want to read:");
+            string fileName = Console.ReadLine();
+
+            string fileContents = File.ReadAllText(fileName);
+
+            List<VideoGame> videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContents);
+
+            if(videoGames.Count > 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Loaded games are:");
+                foreach(VideoGame videoGame in videoGames)
+                {
+                    Console.WriteLine(videoGame);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No games are presnet in the input file.");
+            }
         }
     }
 }
---
+++ GameDataParser/GameDataParser/VideoGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace GameDataParser
+{
+    internal class VideoGame
+    {
+            public string Title { get; init; }
+            public int ReleaseYear { get; init; }
+            public double Rating { get; init; }
+
+        public override string ToString()
+        {
+            return $"{Title} is released in {ReleaseYear} and has a rating of {Rating}";
+        }
+    }
+}
